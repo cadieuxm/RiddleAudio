7d6575404b2a6ea986ca6a51bf994a847d60faff
Michael <mcadieux0730@gmail.com>
2026-05-30 20:27:00 -0500

    style updates

--- js/nav.js
+++ js/nav.js
@@ -1,25 +1,25 @@
 document.write('\
-<nav class="navbar navbar-expand-lg nav-bg">\
+<nav class="navbar navbar-expand-lg riddle-primary-bg">\
   <div class="container-fluid">\
-    <a class="navbar-brand" href="#"><img src="assets/RA_Logo_DropShadow_Cropped.png" width="40" height="40" class="d-inline-block align-text-top" alt="Riddle Audio Logo"></a>\
+    <a class="navbar-brand" href="index.html"><img src="assets/RA_Logo_DropShadow_Cropped.png" width="40" height="40" class="d-inline-block align-text-top" alt="Riddle Audio Logo"></a>\
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
       <span class="navbar-toggler-icon"></span>\
     </button>\
     <div class="collapse navbar-collapse" id="navbarSupportedContent">\
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">\
         <li class="nav-item dropdown">\
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>\
+          <a class="nav-link dropdown-toggle nav-img" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>\
           <ul class="dropdown-menu">\
-            <li><a class="dropdown-item" href="#">Live Audio & Sound Reinforcement</a></li>\
-            <li><hr class="dropdown-divider"></li>\
-            <li><a class="dropdown-item" href="#">Mobile & On-Location Recording</a></li>\
+            <li><a class="dropdown-item nav-img" href="#">Live Audio & Sound Reinforcement</a></li>\
+            <li><hr class="dropdown-divider "></li>\
+            <li><a class="dropdown-item nav-img" href="#">Mobile & On-Location Recording</a></li>\
           </ul>\
         </li>\
-        <li class="nav-item">\
-          <a class="nav-link" href="#">Link</a>\
+        <li class="nav-item ">\
+          <a class="nav-link nav-img" href="#">Link</a>\
         </li>\
         <li class="nav-item">\
-          <a class="nav-link active" aria-current="page" href="#">About Me</a>\
+          <a class="nav-link nav-img" aria-current="page" href="#">About me</a>\
         </li>\
       </ul>\
     </div>\
