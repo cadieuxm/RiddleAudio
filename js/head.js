4f822ce1643c9d80f8ad9e7204b88080a93efdb0
Michael <mcadieux0730@gmail.com>
2026-05-30 22:09:48 -0500

    fixed navbar, added basic about page

---
+++ js/head.js
@@ -0,0 +1,9 @@
+document.write('\<meta charset="utf-8">\
+        <meta name="viewport" content="width=device-width, initial-scale=1">\
+        <title>Riddle Audio - Home</title>\
+        <link rel="icon" href = "assets/RA_Logo_DropShadow_Cropped.png">\
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">\
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>\
+        <link rel="stylesheet" href="main.css"></link>\
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+');
--- js/nav.js
+++ js/nav.js
@@ -1,28 +1,66 @@
 document.write('\
-<nav class="navbar navbar-expand-lg riddle-primary-bg">\
+<nav class="navbar navbar-light navbar-expand-lg riddle-primary-bg">\
   <div class="container-fluid">\
     <a class="navbar-brand" href="index.html"><img src="assets/RA_Logo_DropShadow_Cropped.png" width="40" height="40" class="d-inline-block align-text-top" alt="Riddle Audio Logo"></a>\
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
       <span class="navbar-toggler-icon"></span>\
     </button>\
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+    <div class="collapse navbar-collapse riddle-primary-bg" id="navbarSupportedContent">\
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">\
-        <li class="nav-item dropdown">\
-          <a class="nav-link dropdown-toggle nav-img" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>\
-          <ul class="dropdown-menu">\
+        <li class="nav-item dropdown riddle-primary-bg">\
+          <a class="nav-link dropdown-toggle nav-img riddle-primary-bg" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>\
+          <ul class="dropdown-menu riddle-primary-bg">\
             <li><a class="dropdown-item nav-img" href="#">Live Audio & Sound Reinforcement</a></li>\
-            <li><hr class="dropdown-divider "></li>\
             <li><a class="dropdown-item nav-img" href="#">Mobile & On-Location Recording</a></li>\
           </ul>\
         </li>\
         <li class="nav-item ">\
-          <a class="nav-link nav-img" href="#">Link</a>\
+          <a class="nav-link nav-img riddle-primary-bg" href="#">Link</a>\
         </li>\
         <li class="nav-item">\
-          <a class="nav-link nav-img" aria-current="page" href="#">About me</a>\
+          <a class="nav-link nav-img riddle-primary-bg" aria-current="page" href="aboutme.html">About me</a>\
         </li>\
       </ul>\
     </div>\
   </div>\
 </nav>\
 ');
+
+// document.write('\
+//     <nav class="navbar navbar-light navbar-expand-lg bg-body-tertiary">\
+//   <div class="container-fluid">\
+//     <a class="navbar-brand" href="#">Navbar</a>\
+//     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+//       <span class="navbar-toggler-icon"></span>\
+//     </button>\
+//     <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+//       <ul class="navbar-nav me-auto mb-2 mb-lg-0">\
+//         <li class="nav-item">\
+//           <a class="nav-link active" aria-current="page" href="#">Home</a>\
+//         </li>\
+//         <li class="nav-item">\
+//           <a class="nav-link" href="#">Link</a>\
+//         </li>\
+//         <li class="nav-item dropdown">\
+//           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">\
+//             Dropdown\
+//           </a>\
+//           <ul class="dropdown-menu">\
+//             <li><a class="dropdown-item" href="#">Action</a></li>\
+//             <li><a class="dropdown-item" href="#">Another action</a></li>\
+//             <li><hr class="dropdown-divider"></li>\
+//             <li><a class="dropdown-item" href="#">Something else here</a></li>\
+//           </ul>\
+//         </li>\
+//         <li class="nav-item">\
+//           <a class="nav-link disabled" aria-disabled="true">Disabled</a>\
+//         </li>\
+//       </ul>\
+//       <form class="d-flex" role="search">\
+//         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>\
+//         <button class="btn btn-outline-success" type="submit">Search</button>\
+//       </form>\
+//     </div>\
+//   </div>\
+// </nav>\
+// ');
